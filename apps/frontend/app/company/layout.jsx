7da12cad0f1e1b0be654e26f4bd4a377a1d7add2
Sathurjan <sathu@cognitix.com>
2026-03-09 14:27:21 +0530

    Add frontend authentication and dashboard

---
+++ apps/frontend/app/company/layout.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+export default function CompanyLayout({ children }) {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex h-screen bg-[#f3f4f6] overflow-hidden">
+      <Sidebar section="company" />
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden relative">
+        <Navbar pathname={pathname} />
+        <main className="flex-1 overflow-y-auto p-8">{children}</main>
+      </div>
+    </div>
+  );
+}
